15b1aef49300c7a8007a168ff02e3591d86b5f2f
Franck <franck.roland@kooblit.com>
2014-06-13 09:03:20 +0200

    Tentative de mise en place du formulaire de recherche (echec)

--- search_engine/urls.py
+++ search_engine/urls.py
@@ -2,13 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'kooblit.views.homepage', name='homepage'),
-    url(r'^search/', include('search_engine.urls')),
+    url(r'^(?P<title>[ \w]{0,50})$', 'search_engine.search_views.search_view', name='search_view'),
     # url(r'^blog/', include('blog.urls')),
     #url(r'^admin/', include(admin.site.urls)),
     
-) + static(settings.STATIC_URL, settings.STATIC_ROOT)
+)
