6f0dada7b9ca1e13893c58c7a9385e570ef62043
Franck <franck.roland@kooblit.com>
2014-06-27 08:15:39 +0200

    Changement mode manage + urls contact + grappelli

--- kooblit/urls.py
+++ kooblit/urls.py
@@ -6,11 +6,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'kooblit.views.homepage', name='homepage'),
     url(r'^(?P<essai>\d{1})$', 'kooblit.views.homepage', name='homepage'),
     url(r'^search/', include('search_engine.urls', namespace='search_engine')),
-    # url(r'^blog/', include('blog.urls')),
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^contact/', include('usr_management.urls')),
     
 )
 # + static(settings.STATIC_URL, settings.STATIC_ROOT)
