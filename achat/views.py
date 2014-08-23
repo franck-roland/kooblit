9338b38d2fbb36aadfd4f3f73a92c3671b4de40d
endoderconic <endoderconic@endovm.(none)>
2014-08-23 19:17:40 +0200

    Test de pre-commit avec pep8

--- achat/views.py
+++ achat/views.py
@@ -14,18 +14,18 @@ from django.views.decorators.http import require_GET
 
 
 def cart_details(request):
-	if request.method == 'POST':
-		if request.POST.get('synthese_id',''):
-			if request.POST['synthese_id'] in request.session.get('cart',''):
-				request.session['cart'].remove(request.POST['synthese_id'])
-				request.session.modified = True
-				request.nbre_achats -= 1
-		else:
-			raise Http404()
-	cart = request.session.get('cart',[])
-	cart = ["".join(( "Kooblit de ",Book.objects.get(id=Syntheses.objects.get(id=i).livre_id).title, " par ",
-		Syntheses.objects.get(id=i).user.username)) for i in cart]
-	cart_ids = request.session.get('cart',[])
-	results = zip(cart, cart_ids)
-	return render_to_response('cart.html', RequestContext(request,{'results': results}))
-	
+    print request
+    if request.method == 'POST':
+        if request.POST.get('synthese_id', ''):
+            if request.POST['synthese_id'] in request.session.get('cart', ''):
+                request.session['cart'].remove(request.POST['synthese_id'])
+                request.session.modified = True
+                request.nbre_achats -= 1
+        else:
+            raise Http404()
+    cart = request.session.get('cart', [])
+    cart = ["".join(("Kooblit de ", Book.objects.get(id=Syntheses.objects.get(id=i).livre_id).title, " par ",
+        			Syntheses.objects.get(id=i).user.username)) for i in cart]
+    cart_ids = request.session.get('cart', [])
+    results = zip(cart, cart_ids)
+    return render_to_response('cart.html', RequestContext(request, {'results': results}))
