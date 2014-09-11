b3407e10de252801f83254d21a254ddc58ce643c
endoderconic <endoderconic@endovm.(none)>
2014-09-11 18:08:16 +0200

    Suppression de ligne inutile et de print

--- kooblit/views.py
+++ kooblit/views.py
@@ -1,17 +1,11 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.core.context_processors import csrf
 import random
 
 from django.template import RequestContext
 
-# @login_required(login_url='/contact/')
+
 def homepage(request, essai=""):
-#    c = {}
-    print request
-    try:
-        u = {'kooblit_username': request.user.username}
-    except Exception, e:
-        u = {}
-        raise e
-    return render_to_response("homepage.html",RequestContext(request, u))
+    u = {'kooblit_username': request.user.username}
+    return render_to_response("homepage.html", RequestContext(request, u))
