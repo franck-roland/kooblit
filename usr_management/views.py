20098a61c4f99cb01c5da64f8c5b2d2aaa6949a8
endoderconic <endoderconic@endovm.(none)>
2014-09-11 21:31:19 +0200

    Ajout ajax-loader Ajout de la page de lecture de synthese si l'on a acheté la synthese

--- usr_management/views.py
+++ usr_management/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreationFormKooblit, ReinitialisationForm, DoReinitialisationForm
 from django.contrib.auth.models import User
-from .models import Verification, UserKooblit, Reinitialisation
+from .models import Verification, UserKooblit, Reinitialisation, Syntheses
 from django.contrib.auth import authenticate, login
 from django.utils.datastructures import MultiValueDictKeyError
 from django.contrib.auth import logout
@@ -20,10 +20,10 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 
-#Messages
+# Messages
 from django.contrib import messages
 
-#JSON
+# JSON
 import json
 
 from django.template import RequestContext
@@ -32,28 +32,31 @@ from django.template import RequestContext
 def computeEmail(username, email, validation_id):
     htmly = get_template('email.html')
     d = Context({'username': username, 'validation_id': validation_id})
-    subject, from_email, to = ('Welcome to Kooblit!!', 
-                                'noreply@kooblit.com', email)
+    subject, from_email, to = ('Welcome to Kooblit!!',
+                               'noreply@kooblit.com', email)
     html_content = htmly.render(d)
     msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
     msg.content_subtype = "html"
     msg.send()
 
+
 def computeEmail_reinitialisation(username, email, validation_id):
     htmly = get_template('email_reinitialisation.html')
     d = Context({'username': username, 'validation_id': validation_id})
-    subject, from_email, to = ('[Kooblit] Réinitialisation de mot de passe', 
-                                'noreply@kooblit.com', email)
+    subject, from_email, to = ('[Kooblit] Réinitialisation de mot de passe',
+                               'noreply@kooblit.com', email)
     html_content = htmly.render(d)
     msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
     msg.content_subtype = "html"
     msg.send()
+
 
 def computeNewValidation(username):
     user = UserKooblit.objects.get(username__iexact=username)
     val = Verification(user=user, verification_id=hashlib.sha256(username).hexdigest())
     val.save()
     return val
+
 
 def try_login(request, username, password, next_url):
     try:
@@ -67,7 +70,7 @@ def try_login(request, username, password, next_url):
                 messages.error(request, "Votre compte est désactivé.")
             else:
                 messages.warning(request, "Vous devez activer votre compte.")
-            return HttpResponseRedirect('/',RequestContext(request))
+            return HttpResponseRedirect('/', RequestContext(request))
         else:
             return HttpResponse("Mauvais mot de passe ou identifiant")
     except MultiValueDictKeyError, e:
@@ -76,6 +79,7 @@ def try_login(request, username, password, next_url):
         return HttpResponse("Mauvais mot de passe ou identifiant")
     except Exception, e:
         raise
+
 
 def contact(request):
     try:
@@ -86,8 +90,8 @@ def contact(request):
         raise
 
     if not request.user.is_authenticated():
-        if request.method == 'POST' : # If the form has been submitted...
-            form = UserCreationFormKooblit(request.POST) # A form bound to the POST data
+        if request.method == 'POST':  # If the form has been submitted...
+            form = UserCreationFormKooblit(request.POST)  # A form bound to the POST data
 
             # Check if it's a login
             try:
@@ -98,9 +102,9 @@ def contact(request):
                 pass
             except Exception, e:
                 raise
-                    
+
             # New user
-            if form.is_valid(): # All validation rules pass
+            if form.is_valid():  # All validation rules pass
                 username = form.cleaned_data.get("username")
                 password = form.cleaned_data.get("password1")
                 email = form.cleaned_data.get("email")
@@ -109,17 +113,34 @@ def contact(request):
                 computeEmail(username, email, val.verification_id)
                 messages.success(request, "Félicitation. Un email de confirmation vous a été envoyé.\
                     Vous ne pourrez vous connecter qu'après y avoir jeté un coup d'oeil")
-                return HttpResponseRedirect(next_url,RequestContext(request))
+                return HttpResponseRedirect(next_url, RequestContext(request))
 
         else:
-            form = UserCreationFormKooblit() # An unbound form
+            form = UserCreationFormKooblit()  # An unbound form
 
         return render(request, 'contact.html', {
-            'form': form,'next_url': next_url,
+            'form': form, 'next_url': next_url,
             })
     else:
         return HttpResponseRedirect(next_url)
 
+
+# Lecture des syntheses
+def can_read(id_synthese, username):
+    synthese = Syntheses.objects.get(id=id_synthese)
+    buyer = UserKooblit.objects.get(username=username)
+    return synthese.user.username == username or synthese in buyer.syntheses.all()
+
+
+@login_required
+def lire_synthese(request, synthese_id):
+    username = request.user.username
+    if can_read(synthese_id, username):
+        synt = Syntheses.objects.get(id=synthese_id)
+        resume = synt._file_html.read()
+        return render(request, 'lecture.html', RequestContext(request, {'resume': resume}))
+    else:
+        raise Http404()
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
@@ -131,6 +152,7 @@ def user_logout(request):
     # Take the user back to the homepage.
     return HttpResponseRedirect('/')
 
+
 def email_confirm(request, verification_id):
     try:
         val = Verification.objects.get(verification_id=verification_id)
@@ -140,47 +162,49 @@ def email_confirm(request, verification_id):
             username = val.user.username
             password = val.user.password
             val.delete()
-            
 
             user = User.objects.get(username__iexact=username)
             if user is not None:
                 if user.is_active:
                     user.backend = 'django.contrib.auth.backends.ModelBackend'
                     login(request, user)
-                    messages.success(request,'Votre compte Kooblit est activé!')
-                    return HttpResponseRedirect('/',RequestContext(request))
-            messages.error(request,"Votre compte kooblit est désactivé.")
-            return HttpResponseRedirect('/',RequestContext(request))
+                    messages.success(request, 'Votre compte Kooblit est activé!')
+                    return HttpResponseRedirect('/', RequestContext(request))
+            messages.error(request, "Votre compte kooblit est désactivé.")
+            return HttpResponseRedirect('/', RequestContext(request))
         else:
-            messages.error(request,"Votre compte kooblit est désactivé.")
-            return HttpResponseRedirect('/',RequestContext(request))
+            messages.error(request, "Votre compte kooblit est désactivé.")
+            return HttpResponseRedirect('/', RequestContext(request))
     except Verification.DoesNotExist, e:
         raise Http404()
+
 
 @login_required
 def user_suppression(request):
     user = request.user
     user.delete()
-    return HttpResponseRedirect('/') # Redirect after POST
+    return HttpResponseRedirect('/')  # Redirect after POST
+
 
 @login_required
 def user_profil(request, username):
     user_kooblit = UserKooblit.objects.get(username__iexact=username)
-    if user_kooblit.is_active and user_kooblit.is_confirmed:   
+    if user_kooblit.is_active and user_kooblit.is_confirmed:
         return render(request, 'dashboard/index_profile.html', RequestContext(request, {'user_kooblit': user_kooblit}))
     else:
         raise Http404()
+
 
 def check_exist(request):
     response_data = {}
     response_data['result'] = 'success'
     if request.method == 'GET':
         kwargs = {}
-        username = request.GET.get('username','')
+        username = request.GET.get('username', '')
         if not username:
-            mail = request.GET.get('email','')
+            mail = request.GET.get('email', '')
             if not mail:
-                return HttpResponse(json.dumps(response_data), content_type="application/json")    
+                return HttpResponse(json.dumps(response_data), content_type="application/json")
             kwargs['email'] = mail
         else:
             kwargs['username__iexact'] = username
@@ -191,8 +215,9 @@ def check_exist(request):
         except UserKooblit.DoesNotExist, e:
             pass
         except Exception:
-            raise    
+            raise
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
 
 def ask_reinitialisation(request):
     if request.method == 'POST':
@@ -206,8 +231,8 @@ def ask_reinitialisation(request):
                 reinit.delete()
             try:
                 while True:
-                    x = randrange(2**64)
-                    rnd = hashlib.sha1(''.join((email,str(x)))).hexdigest()
+                    x = randrange(2 ** 64)
+                    rnd = hashlib.sha1(''.join((email, str(x)))).hexdigest()
                     Reinitialisation.objects.get(rnd=rnd)
             except Reinitialisation.DoesNotExist:
                 pass
@@ -216,13 +241,14 @@ def ask_reinitialisation(request):
             r.save()
             computeEmail_reinitialisation(user.username, email, rnd)
             messages.success(request, "Un email vous a été renvoyé")
-            return HttpResponseRedirect('/',RequestContext(request))
+            return HttpResponseRedirect('/', RequestContext(request))
         else:
-            return render(request, 'ask_reinitialisation.html', RequestContext(request, {'form': form}))    
+            return render(request, 'ask_reinitialisation.html', RequestContext(request, {'form': form}))
 
     else:
         form = ReinitialisationForm()
         return render(request, 'ask_reinitialisation.html', RequestContext(request, {'form': form}))
+
 
 def do_reinitialisation(request, r_id):
     try:
@@ -240,5 +266,5 @@ def do_reinitialisation(request, r_id):
             user.save()
             reinit.delete()
             messages.success(request, 'Votre mot de passe a bien été changé')
-            return HttpResponseRedirect('/',RequestContext(request))
+            return HttpResponseRedirect('/', RequestContext(request))
         return render(request, 'do_reinitialisation.html', RequestContext(request, {'form': form}))
