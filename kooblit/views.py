6d0530e36c0b45db8a67b160af185735c1777fee
dev-kooblit <franck.roland@kooblit.com>
2014-10-27 09:08:48 +0100

    Ajout d'une HP alternative Background eclairci Search bar au milieu

--- kooblit/views.py
+++ kooblit/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
-def homepage(request, essai=""):
+def homepage(request, alt=""):
     u = {'kooblit_username': request.user.username}
-    return render_to_response("homepage.html", RequestContext(request, u))
+    if alt:
+        return render_to_response("alt/homepage.html", RequestContext(request, u))    
+    else:
+        return render_to_response("homepage.html", RequestContext(request, u))
