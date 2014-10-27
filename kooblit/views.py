25a2b2d4a0ab55d243f9ad8fe592422528a8d892
dev-kooblit <franck.roland@kooblit.com>
2014-10-27 10:33:41 +0100

    Ajout des dernières synthèses publiées Smooth Scroll Barre de recherche

--- kooblit/views.py
+++ kooblit/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from usr_management.models import Syntheses
 
 def homepage(request, alt=""):
     u = {'kooblit_username': request.user.username}
     if alt:
-        return render_to_response("alt/homepage.html", RequestContext(request, u))    
+        content = [(s,0) for s in Syntheses.objects.order_by("-date")[:4]]
+        u['content'] = content
+        return render_to_response("alt/homepage.html", RequestContext(request, u))
     else:
         return render_to_response("homepage.html", RequestContext(request, u))
