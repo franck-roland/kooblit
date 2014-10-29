3d0e73096d97d6a9ba151c3efb744c527f82fbb2
dev-kooblit <franck.roland@kooblit.com>
2014-10-29 09:07:33 +0100

    Return 500 response in case of error on pdf download + Correction of an indentation error

--- usr_management/views.py
+++ usr_management/views.py
@@ -5,7 +5,7 @@ import re
 import subprocess
 
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import HttpResponseRedirect, HttpResponse, Http404, HttpResponseServerError
 
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreationFormKooblit, ReinitialisationForm, DoReinitialisationForm, AddressChangeForm
@@ -175,14 +175,14 @@ def download_pdf(request, synthese_id):
         html_name = pdf_name+'.html'
 
         open(html_name,'w').write(contenu)
-	args = ["wkhtmltopdf",
+        args = ["wkhtmltopdf",
             "--encoding", "utf-8",
             "--header-html", "127.0.0.1/static/html/pdf_header.html ",
             "--footer-html", "127.0.0.1/static/html/pdf_footer.html",
             html_name, pdf_name,]
         a = subprocess.call(args)
         if a:
-            raise Exception("aaaa")
+            return HttpResponseServerError()
        
         f = FileWrapper(file(pdf_name))
         response = HttpResponse(f, content_type='application/pdf')
