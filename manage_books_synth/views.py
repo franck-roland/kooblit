3860e9cf527d166ee1b37ec5231e12d22d7750a3
dev-kooblit <franck.roland@kooblit.com>
2014-11-03 17:47:14 +0100

    Ajout bouton de retour + Catch du setting du prix

--- manage_books_synth/views.py
+++ manage_books_synth/views.py
@@ -103,7 +103,7 @@ def get_tmp_medium_file(book_title, username):
 
 
 
-def create_file_medium(request, s, book_title, username, has_been_published=False):
+def create_file_medium(request, s, book_title, username, prix=2, has_been_published=False):
     create_book_if_doesnt_exist(request, book_title)
     user = UserKooblit.objects.get(username=username)
     book = Book.objects.get(title=book_title)
@@ -131,6 +131,7 @@ def create_file_medium(request, s, book_title, username, has_been_published=Fals
             synthese = Syntheses(_file_html=File(destination),
                                  user=user, livre_id=book.id, book_title=book.title,
                                  prix=2, has_been_published=has_been_published)
+        synthese.prix = prix
         synthese.save()
         synthese.publish()
         create_pdf.delay(username, synthese)
@@ -211,12 +212,14 @@ def upload_medium(request, book_title):
     user = UserKooblit.objects.get(username=username)
     if request.method == 'POST':
         if "_publish" in request.POST:
-            create_file_medium(request, request.POST['q'], book_title, username, has_been_published=True)
+            price = float(request.POST['prix'])
+            create_file_medium(request, request.POST['q'], book_title, username, price,  has_been_published=True)
             messages.success(request, u'Votre synthèse pour le livre <i>"%s"</i> a bien été publié.' % book_title)
             send_alert(book_title)
             return HttpResponseRedirect('/', request)
         elif "_quit" in request.POST:
-            create_file_medium(request, request.POST['q'], book_title, username)
+            price = float(request.POST['prix'])
+            create_file_medium(request, request.POST['q'], book_title, username, price)
             messages.success(request, u'Votre synthèse pour le livre <i>"%s"</i> a bien été enregistré.' % book_title)
             return HttpResponseRedirect('/', request)
         else:
