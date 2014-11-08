6ec541a611908afe2d83a627b9edd56fa9191cea
dev-kooblit <franck.roland@kooblit.com>
2014-11-08 10:06:51 +0100

    Alignement responsive de la barre de nav + ajout des synthèses en fin + ajout du lien pour aller a la fin

--- kooblit/views.py
+++ kooblit/views.py
@@ -7,9 +7,9 @@ from kooblit_lib.utils import get_content_for_template
 
 def homepage(request, alt=""):
     u = {'kooblit_username': request.user.username}
+    syntheses = Syntheses.objects.order_by("-date")
+    u['syntheses'] = syntheses
     if alt:
-        syntheses = Syntheses.objects.order_by("-date")
-        u['syntheses'] = syntheses
         return render_to_response("alt/homepage.html", RequestContext(request, u))
     else:
         return render_to_response("homepage.html", RequestContext(request, u))
