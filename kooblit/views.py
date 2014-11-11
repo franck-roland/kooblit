8aa1cc4099b7882da8b2a3dc251a00078ecfa7df
franck-l-roland <ubuntu@ip-172-31-13-86.eu-west-1.compute.internal>
2014-11-11 09:56:36 +0000

    Rajout de la recherche de synthèses hp

--- kooblit/views.py
+++ kooblit/views.py
@@ -4,11 +4,16 @@ from django.template import RequestContext
 from usr_management.models import Syntheses
 
 from kooblit_lib.utils import get_content_for_template
+import time
+import sys
 
 def homepage(request, alt=""):
     u = {'kooblit_username': request.user.username}
+    st = time.clock()
     syntheses = Syntheses.objects.order_by("-date")[:4]
     u['syntheses'] = syntheses
+    st = time.clock() - st
+    print >> sys.stderr, st
     if alt:
         return render_to_response("alt/homepage.html", RequestContext(request, u))
     else:
