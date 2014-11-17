4ed8cdbfe4fd59e8bcd0c3562abf9a8204b31e0c
franck-l-roland <kooblit@vps71616.ovh.net>
2014-11-17 17:06:02 +0100

    Ajout des retour en cas d'erreur

--- manage_books_synth/tasks.py
+++ manage_books_synth/tasks.py
@@ -1,4 +1,4 @@
-
+import sys
 import os
 import subprocess
 
@@ -15,17 +15,18 @@ from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
+from django.templatetags.static import static
 
 @shared_task
 def create_pdf(username, synth):
     try:
         user = UserKooblit.objects.get(username=username)
         if synth.user != user:
-            return
+            return 1
     except UserKooblit.DoesNotExist:
-        return
+        return 1
     except Syntheses.DoesNotExist:
-        return
+        return 1
 
     contenu = synth.contenu_pdf()
     pdf_name = ''.join(('/tmp/synth_', str(synth.id), '_', str(synth.version)))
@@ -33,15 +34,17 @@ def create_pdf(username, synth):
     with open(html_name,'w') as f_html:
         f_html.write(contenu)
 
+    url_header = '127.0.0.1/static/html/pdf_header.html'
+    url_footer = '127.0.0.1/static/html/pdf_footer.html'
     args = ["wkhtmltopdf",
         "--encoding", "utf-8",
-        "--header-html", "127.0.0.1/static/html/pdf_header.html ",
-        "--footer-html", "127.0.0.1/static/html/pdf_footer.html",
+        "--header-html", url_header,
+        "--footer-html", url_footer, 
         html_name, pdf_name,]
 
     a = subprocess.call(args)
     if a:
-        return
+        return a
     with open(pdf_name, 'rb') as f_pdf:
         synth.file_pdf = File(f_pdf)
         synth.save()
