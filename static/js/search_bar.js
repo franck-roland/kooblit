4a3da4ac43046af2e910568652f32448e25bfc9f
dev-kooblit <franck.roland@kooblit.com>
2014-11-20 19:33:07 +0100

    Ajout de l'autocompletion a la page d'acceuil

--- static/js/search_bar.js
+++ static/js/search_bar.js
@@ -22,64 +22,94 @@ var substringMatcher = function(strs) {
   };
 };
 
-
 var livres = [];
-$('#search_bar').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 3
-},
-{
-  name: 'livres',
-  displayKey: function(s){ return s.toString();},
-  source: function (request, cb) {
-                $.ajax({
+function set_search_bar(search_name, insert_after){
+    $(search_name).attr('autocomplete', 'off');
+    var $autocomplete = $("<ul id='autocomplete'></ul>").hide().insertAfter(insert_after);
+    var selectedItem = null;
+
+    var populateSearchField = function(escape){
+        $(search_name).val($autocomplete.find('li').eq(selectedItem).text());
+        if(escape){
+            setSelectedItem(null);
+        }
+    }
+    var setSelectedItem = function(item){
+        selectedItem = item;
+        if (selectedItem === null){
+            $autocomplete.hide();
+            return;
+        }
+        else if(selectedItem < 0){
+            selectedItem = 0;
+        }
+        else if(selectedItem >= $autocomplete.find('li').length){
+            selectedItem = $autocomplete.find('li').length - 1;
+        }
+        $autocomplete.find('li').removeClass('selected').eq(selectedItem).addClass('selected');
+        $autocomplete.show();
+    }
+    $(search_name).keypress(function(event){
+        if(event.keyCode == 13 && selectedItem !== null){
+            populateSearchField(true);
+        }
+    });
+    $(search_name).keyup(function(event){
+        if(event.keyCode > 40 || event.keyCode == 8){
+            $.ajax({
                     url: "http://completion.amazon.co.uk/search/complete",
                     type: "GET",
                     cache: false,
                     dataType: "jsonp",
                     success: function (data) {
-                        livres = data[1];
-                        cb($.makeArray(data[1]));
+                        console.log(data[1]);
+                        if(data[1].length){
+                            $autocomplete.empty();
+                            $.each(data[1], function(index, term) {
+                                $('<li></li>').text(term).appendTo($autocomplete)
+                                .mouseover(function(){
+                                    setSelectedItem(index);
+                                })
+                                .click(function(){
+                                    $(search_name).val(term);
+                                    $autocomplete.hide();
+                                });
+                            });
+                            var len = $autocomplete.find('li').length;
+                            $($autocomplete.find('li')[0]).addClass("first_list");
+                            $($autocomplete.find('li')[len-1]).addClass("last_list");
+
+                            setSelectedItem(0);
+                        }
+                        else{
+                            setSelectedItem(null);   
+                        }
                     },
                     data: {
-                        q: request,
+                        q: $(search_name).val(),
                         "search-alias": "stripbooks",
                         mkt: "5",
-                        callback: '?'
                     }
-                });
-            }
-},
-{
-  name: 'livres_anglais',
-  displayKey: function(s){ return s.toString();},
-  source: function (request, cb) {
-                $.ajax({
-                    url: "http://completion.amazon.co.uk/search/complete",
-                    type: "GET",
-                    cache: false,
-                    dataType: "jsonp",
-                    success: function (data) {
-                        var temp = [];
-                        for (var l in data[1]){
-                            if (livres.indexOf(data[1][l]) == -1){
-                                temp.push(data[1][l]);
-                            }
-                        }
-                        if (!$('.divider').length){
-                            $('.tt-dataset-livres').after('<div class="divider"></div>');
-                        }
-                        cb(temp);
-
-                    },
-                    data: {
-                        q: request,
-                        "search-alias": "english-books",
-                        mkt: "5",
-                        callback: '?'
-                    }
-                });
-            }
+            });
+        }
+        else if (event.keyCode == 38 && selectedItem !== null){
+            //haut
+            setSelectedItem(selectedItem - 1);
+            populateSearchField(false);
+        }
+        else if (event.keyCode == 40 && selectedItem !== null){
+            //haut
+            setSelectedItem(selectedItem + 1);
+            populateSearchField(false);
+        }
+        else if (event.keyCode == 27 && selectedItem !== null){
+            //echap
+            setSelectedItem(null);
+        }
+    });
+    $(search_name).blur(function(event){
+        setTimeout(function(){
+            setSelectedItem(null);
+        }, 250);
+    })
 }
-);
