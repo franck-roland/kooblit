c9595e5abd743e193bf82d228ed8ad5c07f3c109
dev-kooblit <franck.roland@kooblit.com>
2014-11-21 08:48:44 +0100

    Correction bug sur templateTags bought

--- usr_management/templatetags/synth_tags.py
+++ usr_management/templatetags/synth_tags.py
@@ -3,21 +3,21 @@ from usr_management.models import UserKooblit, Version_Synthese
 register = template.Library()
 
 @register.filter(name='bought')
-def bought(value, user):
+def bought(synthese, user):
     if not user.is_authenticated():
         return False
     else:
         try:
-            current_user = user
-            if value.user == current_user: # Auteur de la synthese
+            current_user = user.userkooblit
+            if synthese.user == current_user: # Auteur de la synthese
                 return True
-            if Version_Synthese.objects.filter(userkooblit=current_user, synthese=value): # Dans les achats
+            if current_user.syntheses_achetees.filter(synthese=synthese): # Dans les achats
                 return True
             else:
                 return False
         except Exception, e:
             return False
-    return user.is_authenticated() and not value.can_be_added_by(user.username)
+
 
 @register.filter(name='can_note')
 def can_note(user, synth):
